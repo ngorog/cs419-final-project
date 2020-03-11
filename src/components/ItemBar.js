dc9125dadf1c6d6235746e6c55055f45645f3b51
dongrog <dongrog@oregonstate.edu>
2020-03-11 14:59:33 -0700

    Shortened the Item Bar code using loop

--- src/components/ItemBar.js
+++ src/components/ItemBar.js
@@ -1,142 +1,48 @@
 import React from "react";
 import { Grid, Image } from "semantic-ui-react";
 
+function getItem(props) {
+	let jsx = [];
+	let items = [];
+	let i = 0;
+	items.push(props.stats.item1);
+	items.push(props.stats.item2);
+	items.push(props.stats.item3);
+	items.push(props.stats.item4);
+	items.push(props.stats.item5);
+	items.push(props.stats.item6);
+
+	for (i = 0; i < 6; i++) {
+		if (items[i] !== 0) {
+			jsx.push(
+				<Image
+					id='item-img'
+					src={`http://ddragon.leagueoflegends.com/cdn/10.5.1/img/item/${items[i]}.png`}
+					rounded
+					floated='left'
+					centered
+					size={props.size}
+				/>
+			);
+		} else {
+			jsx.push(
+				<Image
+					id='item-img'
+					src={process.env.PUBLIC_URL + "/blankitem.png"}
+					floated='left'
+					size={props.size}
+					centered
+				/>
+			);
+		}
+	}
+	return jsx;
+}
+
 function ItemBar(props) {
 	return (
 		<Grid.Column id='user-item' textAlign='center' width={6}>
-			{console.log(props.stats)}
-			{props.stats.item0 !== 0 ? (
-				<Image
-					id='item-img'
-					src={`http://ddragon.leagueoflegends.com/cdn/10.5.1/img/item/${props.stats.item0}.png`}
-					rounded
-					floated='left'
-					centered
-					size={props.size}
-				/>
-			) : (
-				<Image
-					id='item-img'
-					src={process.env.PUBLIC_URL + "/blankitem.png"}
-					floated='left'
-					size={props.size}
-					centered
-				/>
-			)}
-
-			{props.stats.item1 !== 0 ? (
-				<Image
-					id='item-img'
-					src={`http://ddragon.leagueoflegends.com/cdn/10.5.1/img/item/${props.stats.item1}.png`}
-					rounded
-					floated='left'
-					size={props.size}
-					centered
-				/>
-			) : (
-				<Image
-					id='item-img'
-					src={process.env.PUBLIC_URL + "/blankitem.png"}
-					floated='left'
-					size={props.size}
-					centered
-				/>
-			)}
-
-			{props.stats.item2 !== 0 ? (
-				<Image
-					id='item-img'
-					src={`http://ddragon.leagueoflegends.com/cdn/10.5.1/img/item/${props.stats.item2}.png`}
-					rounded
-					floated='left'
-					size={props.size}
-					centered
-				/>
-			) : (
-				<Image
-					id='item-img'
-					src={process.env.PUBLIC_URL + "/blankitem.png"}
-					floated='left'
-					size={props.size}
-					centered
-				/>
-			)}
-
-			{props.stats.item3 !== 0 ? (
-				<Image
-					id='item-img'
-					src={`http://ddragon.leagueoflegends.com/cdn/10.5.1/img/item/${props.stats.item3}.png`}
-					rounded
-					floated='left'
-					size={props.size}
-					centered
-				/>
-			) : (
-				<Image
-					id='item-img'
-					src={process.env.PUBLIC_URL + "/blankitem.png"}
-					floated='left'
-					size={props.size}
-					centered
-				/>
-			)}
-
-			{props.stats.item4 !== 0 ? (
-				<Image
-					id='item-img'
-					src={`http://ddragon.leagueoflegends.com/cdn/10.5.1/img/item/${props.stats.item4}.png`}
-					rounded
-					floated='left'
-					size={props.size}
-					centered
-				/>
-			) : (
-				<Image
-					id='item-img'
-					src={process.env.PUBLIC_URL + "/blankitem.png"}
-					floated='left'
-					size={props.size}
-					centered
-				/>
-			)}
-
-			{props.stats.item5 !== 0 ? (
-				<Image
-					id='item-img'
-					src={`http://ddragon.leagueoflegends.com/cdn/10.5.1/img/item/${props.stats.item5}.png`}
-					rounded
-					floated='left'
-					size={props.size}
-					centered
-				/>
-			) : (
-				<Image
-					id='item-img'
-					src={process.env.PUBLIC_URL + "/blankitem.png"}
-					floated='left'
-					size={props.size}
-					centered
-				/>
-			)}
-
-			{props.stats.item6 !== 0 ? (
-				<Image
-					id='item-img'
-					src={`http://ddragon.leagueoflegends.com/cdn/10.5.1/img/item/${props.stats.item6}.png`}
-					rounded
-					floated='left'
-					size={props.size}
-					centered
-				/>
-			) : (
-				<Image
-					id='item-img'
-					src={process.env.PUBLIC_URL + "/blankitem.png"}
-					floated='left'
-					size={props.size}
-					centered
-				/>
-			)}
+			{getItem(props)}
 		</Grid.Column>
 	);
 }
