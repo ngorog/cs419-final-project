43daf68bafe2e6e5ef4031731119136b6a42863d
dongrog <dongrog@oregonstate.edu>
2020-03-20 06:39:51 -0700

    Added status feature

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,46 +3,56 @@ import { Route, Switch, Link } from "react-router-dom";
 import { Button, Icon, Menu } from "semantic-ui-react";
 
 class NavBar extends React.Component {
-  state = {};
+	state = {};
 
-  handleItemClick = (e, { name }) => this.setState({ activeItem: name });
+	handleItemClick = (e, { name }) => this.setState({ activeItem: name });
 
-  render() {
-    const { activeItem } = this.state;
-    return (
-      <Menu>
-        <Menu.Item
-          as={Link}
-          to="/"
-          name="home"
-          active={activeItem === "home"}
-          onClick={this.handleItemClick}
-        >
-          Home
-        </Menu.Item>
+	render() {
+		const { activeItem } = this.state;
+		return (
+			<Menu>
+				<Menu.Item
+					as={Link}
+					to='/'
+					name='home'
+					active={activeItem === "home"}
+					onClick={this.handleItemClick}
+				>
+					Home
+				</Menu.Item>
 
-        <Menu.Item
-          as={Link}
-          to="/champion"
-          name="champion"
-          active={activeItem === "champion"}
-          onClick={this.handleItemClick}
-        >
-          Champions
-        </Menu.Item>
+				<Menu.Item
+					as={Link}
+					to='/champion'
+					name='champion'
+					active={activeItem === "champion"}
+					onClick={this.handleItemClick}
+				>
+					Champions
+				</Menu.Item>
 
-        <Menu.Item
-          as={Link}
-          to="/item"
-          name="item"
-          active={activeItem === "item"}
-          onClick={this.handleItemClick}
-        >
-          Items
-        </Menu.Item>
-      </Menu>
-    );
-  }
+				<Menu.Item
+					as={Link}
+					to='/item'
+					name='item'
+					active={activeItem === "item"}
+					onClick={this.handleItemClick}
+				>
+					Items
+				</Menu.Item>
+
+				<Menu.Item
+					as={Link}
+					to='/status'
+					name='status'
+					active={activeItem === "status"}
+					onClick={this.handleItemClick}
+				>
+					Server Status
+				</Menu.Item>
+			</Menu>
+		);
+	}
 }
 
 export default NavBar;
---
+++ src/pages/Status.js
@@ -0,0 +1,96 @@
+import React, { createRef } from "react";
+import { Grid, Loader, Dimmer, Header, Label, List } from "semantic-ui-react";
+
+class Status extends React.Component {
+	state = { loaded: false };
+
+	doCORSRequest = (options, setResult) => {
+		var cors_api_url = "https://cors-anywhere.herokuapp.com/";
+		var x = new XMLHttpRequest();
+		return new Promise((resolve, reject) => {
+			x.onload = x.onerror = () => {
+				if (typeof setResult === "function") {
+					setResult(JSON.parse(x.responseText));
+				}
+				this.setState({ status: x.status });
+				resolve(x);
+				return;
+			};
+			this.setState({ status: x.status });
+			x.open(options.method, cors_api_url + options.url);
+			x.send();
+		});
+	};
+
+	onUserClick = user => {
+		this.props.history.push(`/search/${user}`); // or whatever string path
+		window.location.reload();
+	};
+
+	setStatusInfo = result => {
+		this.setState({ statusInfo: result });
+	};
+
+	componentDidMount() {
+		let api_key = "RGAPI-ec3eb8e5-479a-4d79-8c2d-189345c76d14";
+		this.doCORSRequest(
+			{
+				method: "GET",
+				url: `https://na1.api.riotgames.com/lol/status/v3/shard-data?api_key=${api_key}`
+			},
+			this.setStatusInfo
+		).then(() => this.setState({ loaded: true }));
+	}
+
+	contextRef = createRef();
+
+	render() {
+		return this.state.loaded ? (
+			<div>
+				<Grid>
+					<Grid.Row>
+						<Grid.Column textAlign='center'>
+							<Header as='h1'>North America Server Status</Header>
+							<List divided selection>
+								<List.Item>
+									<Label color='green' horizontal>
+										<Header as='h2'>
+											Game {this.state.statusInfo.services[0].status}
+										</Header>
+									</Label>
+								</List.Item>
+								<List.Item>
+									<Label color='green' horizontal>
+										<Header as='h2'>
+											Store {this.state.statusInfo.services[1].status}
+										</Header>
+									</Label>
+								</List.Item>
+								<List.Item>
+									<Label color='green' horizontal>
+										<Header as='h2'>
+											Website {this.state.statusInfo.services[2].status}
+										</Header>
+									</Label>
+								</List.Item>
+								<List.Item>
+									<Label color='green' horizontal>
+										<Header as='h2'>
+											Client {this.state.statusInfo.services[3].status}
+										</Header>
+									</Label>
+								</List.Item>
+							</List>
+						</Grid.Column>
+					</Grid.Row>
+				</Grid>
+			</div>
+		) : (
+			<Dimmer active>
+				<Loader />
+			</Dimmer>
+		);
+	}
+}
+
+export default Status;
